834d26ce954e3924d8819408c36779ba16bb4b5f
Birhanu <birhanumatebe12@gmail.com>
2025-05-04 15:41:05 +0300

    Home page added

--- frontend/UIdev/src/pages/Home.jsx
+++ frontend/UIdev/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import HomeSection from "../components/home-page/HomeSection"
 
 function Home() {
   return (
-    <div className='mt-15 h-screen text-center'>
-    <h1> home page</h1>
-    <h2> Hi Translated our Figma design into a React component and render over here. and remove this text</h2>
+    <div className='mt-15'>
+      <HomeSection/>
+    
    
  
   </div>
