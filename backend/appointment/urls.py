18756f7a43f6c0218801f3895a9771558a353898
Birhanu <birhanumatebe12@gmail.com>
2025-05-15 12:21:44 +0300

    backend folder deleted

--- backend/appointment/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import AppointmentListCreateView, AppointmentCancelView
-
-urlpatterns = [
-    path('', AppointmentListCreateView.as_view(), name='appointment-list-create'),
-    path('<uuid:pk>/cancel/', AppointmentCancelView.as_view(), name='appointment-cancel'),
-]
